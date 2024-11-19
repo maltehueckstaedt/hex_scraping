108f4a0f640bf4a9f1dea2437655f2d91cebf65e
Malte Hückstädt <deaddatascientists@gmail.com>
2024-11-19 13:10:14 +0100

    durchlaufe seite für seite und ziehe htmls

--- R/scraping_wupptertal.r
+++ R/scraping_wupptertal.r
@@ -62,38 +62,57 @@ Suchbegriffe$sendKeysToElement(list(key = "enter"))
 # angeklickt werden und der Sourcecode geladen werden:
 #.:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::s
 
-# Kurs <- rmdr$findElement(using = "xpath", '//*[@id="genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Table:0:row"]/td[3]')
-# Kurs$clickElement()
+page_sources <- list()  # Liste zur Speicherung der Seitenquellen
+current_index <- 1      # Startindex für die Liste `page_sources`
 
-# # Gesamte HTML der Seite auslesen
-# page_source <- rmdr$getPageSource()[[1]]
+# Anzahl der Elemente
+total_elements <- 2612
+elements_per_page <- 10
 
-
-
-
-# Vektor, um die HTML-Inhalte zu speichern
-page_sources <- list()
-
-# Schleife durch die 10 Elemente (Index von 0 bis 9)
-for (i in 0:9) {
-  # XPath für das aktuelle Element dynamisch erstellen
-  xpath_kurs <- sprintf('//*[@id="genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Table:%d:tableRowAction"]', i)
-
-  # Element finden und anklicken
-  Kurs <- rmdr$findElement(using = "xpath", xpath_kurs)
-  Kurs$clickElement()
+# Schleife über alle Seiten
+for (page in 0:(ceiling(total_elements / elements_per_page) - 1)) {
   
-  # HTML der Seite auslesen und speichern
-  page_source <- rmdr$getPageSource()[[1]]
-  page_sources[[i + 1]] <- page_source
+  # Start- und Endindex für die aktuelle Seite berechnen
+  start_index <- page * elements_per_page
+  end_index <- min(start_index + elements_per_page - 1, total_elements - 1)
   
-  # Auf "Zurück" klicken
-  back_button <- rmdr$findElement(using = "xpath", '//*[@id="form:dialogHeader:backButtonTop"]')
-  back_button$clickElement()
-  Sys.sleep(.5)  # 2 Sekunden warten (kann angepasst werden)
+  # Schleife durch die Elemente auf der aktuellen Seite
+  for (i in start_index:end_index) {
+    # XPath für das aktuelle Element dynamisch erstellen
+    xpath_kurs <- sprintf('//*[@id="genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Table:%d:tableRowAction"]', i)
+
+    tryCatch({
+      # Element finden und anklicken
+      Kurs <- rmdr$findElement(using = "xpath", xpath_kurs)
+      Kurs$clickElement()
+      
+      # HTML der Seite auslesen und speichern
+      page_source <- rmdr$getPageSource()[[1]]
+      page_sources[[current_index]] <- page_source
+      current_index <- current_index + 1  # Index für gespeicherte Seiten erhöhen
+      
+      # Auf "Zurück" klicken
+      back_button <- rmdr$findElement(using = "xpath", '//*[@id="form:dialogHeader:backButtonTop"]')
+      back_button$clickElement()
+      Sys.sleep(.5)  # Kurze Pause zwischen Aktionen
+    }, error = function(e) {
+      # Fehlerbehandlung, wenn ein Element nicht gefunden wird
+      message(sprintf("Fehler bei Element %d auf Seite %d: %s", i, page + 1, e$message))
+    })
+  }
+
+  # Klick auf "Weiter" für die nächste Seite, außer bei der letzten Seite
+  if (end_index < total_elements - 1) {
+    tryCatch({
+      next_button <- rmdr$findElement(using = "xpath", '//*[@id="genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2next"]')
+      next_button$clickElement()
+      Sys.sleep(1)  # Wartezeit, um die nächste Seite vollständig zu laden
+    }, error = function(e) {
+      # Fehlerbehandlung beim Klicken auf "Weiter"
+      message(sprintf("Fehler beim Klicken auf 'Weiter' auf Seite %d: %s", page + 1, e$message))
+    })
+  }
 }
-# Überprüfen der gesammelten HTML-Daten
-str(page_sources)
 
 
 
@@ -104,23 +123,6 @@ str(page_sources)
 
 
 
-
- 
-# page_source <- rmdr$getPageSource() |> unlist()
- 
-elements <- rmdr$findElements(using = 'css selector', value = '.linkTable span')
- 
- 
-# Klicke nacheinander auf jedes Element
-for (i in seq_along(elements)) {
-  elements[[i]]$clickElement()
-  # Optional: Warte zwischen den Klicks (z.B. 2 Sekunden, um eine Seite zu laden)
-  Sys.sleep(2)
-  # Navigiere ggf. wieder zurück, falls du auf eine andere Seite geleitet wirst
-  rmdr$goBack()
-  # Wiederhole das Finden der Elemente, da sich die Seite nach dem Klick geändert haben könnte
-  elements <- rmdr$findElements(using = 'css selector', value = '.linkTable span')
-}
  
   
 driver$server$stop()
