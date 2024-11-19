7bdb81ad685302f046653339388e9f3acea8005e
Malte Hückstädt <deaddatascientists@gmail.com>
2024-11-19 14:51:19 +0100

    upd

--- R/scraping_wupptertal.r
+++ R/scraping_wupptertal.r
@@ -26,7 +26,7 @@ pacman::p_load(tidyverse, rvest, RSelenium)
 #////////////////////////////////////////////////////////////
  
 # Starten einer Remote-Sitzung mit Chrome
-driver <- rsDriver(browser = "chrome", chromever = "125.0.6422.60", port = 4111L)
+driver <- rsDriver(browser = "chrome", chromever = "125.0.6422.60", port = 4231L)
  
 # Zugriff auf die gestartete Sitzung
 rmdr <- driver[["client"]]
@@ -62,57 +62,34 @@ Suchbegriffe$sendKeysToElement(list(key = "enter"))
 # angeklickt werden und der Sourcecode geladen werden:
 #.:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::s
 
-page_sources <- list()  # Liste zur Speicherung der Seitenquellen
-current_index <- 1      # Startindex für die Liste `page_sources`
+page_sources <- list()
 
-# Anzahl der Elemente
-total_elements <- 2612
-elements_per_page <- 10
-
-# Schleife über alle Seiten
-for (page in 0:(ceiling(total_elements / elements_per_page) - 1)) {
+# Schleife durch die 10 Elemente (Index von 0 bis 9)
+for (i in 0:9) {
+  #browser()
+  # XPath für das aktuelle Element dynamisch erstellen
+  xpath_kurs <- sprintf('//*[@id="genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Table:%d:actionsLeft:show:link"]', i)
   
-  # Start- und Endindex für die aktuelle Seite berechnen
-  start_index <- page * elements_per_page
-  end_index <- min(start_index + elements_per_page - 1, total_elements - 1)
-  
-  # Schleife durch die Elemente auf der aktuellen Seite
-  for (i in start_index:end_index) {
-    # XPath für das aktuelle Element dynamisch erstellen
-    xpath_kurs <- sprintf('//*[@id="genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Table:%d:tableRowAction"]', i)
-
-    tryCatch({
-      # Element finden und anklicken
-      Kurs <- rmdr$findElement(using = "xpath", xpath_kurs)
-      Kurs$clickElement()
-      
-      # HTML der Seite auslesen und speichern
-      page_source <- rmdr$getPageSource()[[1]]
-      page_sources[[current_index]] <- page_source
-      current_index <- current_index + 1  # Index für gespeicherte Seiten erhöhen
-      
-      # Auf "Zurück" klicken
-      back_button <- rmdr$findElement(using = "xpath", '//*[@id="form:dialogHeader:backButtonTop"]')
-      back_button$clickElement()
-      Sys.sleep(.5)  # Kurze Pause zwischen Aktionen
-    }, error = function(e) {
-      # Fehlerbehandlung, wenn ein Element nicht gefunden wird
-      message(sprintf("Fehler bei Element %d auf Seite %d: %s", i, page + 1, e$message))
-    })
-  }
-
-  # Klick auf "Weiter" für die nächste Seite, außer bei der letzten Seite
-  if (end_index < total_elements - 1) {
-    tryCatch({
-      next_button <- rmdr$findElement(using = "xpath", '//*[@id="genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2next"]')
-      next_button$clickElement()
-      Sys.sleep(1)  # Wartezeit, um die nächste Seite vollständig zu laden
-    }, error = function(e) {
-      # Fehlerbehandlung beim Klicken auf "Weiter"
-      message(sprintf("Fehler beim Klicken auf 'Weiter' auf Seite %d: %s", page + 1, e$message))
-    })
+  # Versuch, das Element zu finden
+  if (rmdr$findElements(using = "xpath", xpath_kurs) %>% length > 0) {
+    Kurs <- rmdr$findElement(using = "xpath", xpath_kurs)
+    Kurs$clickElement()
+    
+    # HTML der Seite auslesen und speichern
+    page_source <- rmdr$getPageSource()[[1]]
+    page_sources[[i + 1]] <- page_source
+    Sys.sleep(1) 
+    # Auf "Zurück" klicken
+    back_button <- rmdr$findElement(using = "xpath", '//*[@id="form:dialogHeader:backButtonTop"]')
+    back_button$clickElement()
+    Sys.sleep(1)  # Warten, um sicherzustellen, dass die Seite vollständig geladen ist
+  } else {
+    message(sprintf("Element mit Index %d wurde nicht gefunden", i))
   }
 }
+# Überprüfen der gesammelten HTML-Daten
+str(page_sources)
+
 
 
 
