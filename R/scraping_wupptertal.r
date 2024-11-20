b65c03d3bda789e5a448b6cf0acbb291fd9db320
Malte Hückstädt <deaddatascientists@gmail.com>
2024-11-20 12:58:16 +0100

    Restart scraping script from scratch

--- R/scraping_wupptertal.r
+++ R/scraping_wupptertal.r
@@ -60,58 +60,56 @@ Suchbegriffe$sendKeysToElement(list(key = "enter"))
 # Die Vorlesungen werden angezeigt. Immer zehn Stück pro Seite
 # bei insgesamt ca. 200-300 Seiten. Es muss nun jede Seite
 # angeklickt werden und der Sourcecode geladen werden:
-#.:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::s
-# Gesamte Anzahl der Elemente
-total_elements <- 2611
+#.:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# Anzahl der Elemente pro Seite
-elements_per_page <- 10
-
-# Liste zur Speicherung der Seitenquellen
-page_sources <- list()
-
-# Äußerer Loop für Seiten
-for (page in 0:(ceiling(total_elements / elements_per_page) - 1)) {
-  # Innerer Loop für die Elemente auf der aktuellen Seite
-  start_index <- page * elements_per_page
-  print(start_index)
-  end_index <- min(total_elements - 1, start_index + elements_per_page - 1) # Begrenzung auf die letzten Elemente
-  print(end_index)
-  for (i in start_index:end_index) {
-    
-    xpath_kurs <- sprintf('//*[@id="genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Table:%d:actionsLeft:show:link"]', i)
-    
-    # Versuche, das Element zu finden
-    if (rmdr$findElements(using = "xpath", xpath_kurs) %>% length > 0) {
-      Kurs <- rmdr$findElement(using = "xpath", xpath_kurs)
-      Kurs$clickElement()
-      
-      # HTML der Seite auslesen und speichern
-      page_source <- rmdr$getPageSource()[[1]]
-      page_sources[[i + 1]] <- page_source
-      Sys.sleep(1)
-      
-      # Versuche, den "Zurück"-Button zu klicken
-      click_back_button(rmdr)
-      Sys.sleep(1)  # Warten, um sicherzustellen, dass die Seite vollständig geladen ist
-    } else {
-      message(sprintf("Element mit Index %d wurde nicht gefunden", i))
-    }
-  }
-  
-  # "Weiter"-Button klicken, außer bei der letzten Seite
-  if (page < ceiling(total_elements / elements_per_page) - 1) {
-    next_button <- rmdr$findElement(using = "css selector", "#genSearchRes\\:id3df798d58b4bacd9\\:id3df798d58b4bacd9Navi2next")
-    next_button$clickElement()
-    Sys.sleep(2)  # Warten, bis die nächste Seite vollständig geladen ist
-  }
-}
-
-message("Scraping abgeschlossen!")
+# Finde den ersten Kurs über CSS
+kurs <- rmdr$findElement(using = "css selector", '#genSearchRes\\:id3df798d58b4bacd9\\:id3df798d58b4bacd9Table\\:0\\:tableRowAction')
+kurs$clickElement()
 
 
+# finde titel
+titel <- rmdr$findElement(using = "css selector", '#\\31 8a8022569d6ced829f833aa855530ce')
+# ziehe titel
+titel <- titel$getElementText()
+
+
+# finde nummer
+nummer <- rmdr$findElement(using = "css selector", '#a6b7089fcf43a67764ca850c1e4661d5')
+# ziehe nummer
+nummer <- nummer$getElementText()
 
  
-  
+# finde Organisationseinheit
+Organisationseinheit <- rmdr$findElement(using = "css selector", 'ul.listStyleIconSimple:nth-child(1) > li:nth-child(1)')
+# ziehe Organisationseinheit
+Organisationseinheit <- Organisationseinheit$getElementText()
+
+
+# finde Veranstaltungsart
+Veranstaltungsart <- rmdr$findElement(using = "css selector", '#\\34 fc695e29c07ca4ad6b71c515398e8e8')
+# ziehe Organisationseinheit
+Veranstaltungsart <- Veranstaltungsart$getElementText()
+
+
+ # finde Angebotshaeufigkeit
+Angebotshaeufigkeit <- rmdr$findElement(using = "css selector", '#\\37 fad543acae49a98047a57220463ecdd')
+# ziehe Organisationseinheit
+Angebotshaeufigkeit <- Angebotshaeufigkeit$getElementText()
+ 
+# Finde "Inhalte"-Tab und klicke darauf
+kurs <- rmdr$findElement(using = "css selector", '#detailViewData\\:tabContainer\\:term-planning-container\\:tabs\\:contentsTab')
+kurs$clickElement()
+
+# finde Boxtitel
+Boxtitel <- rmdr$findElement(using = "css selector", 'div.box_title:nth-child(2)')
+# ziehe Organisationseinheit
+Boxtitel <- Boxtitel$getElementText()
+
+# finde Boxinhalt
+Boxinhalt <- rmdr$findElement(using = "css selector", '#detailViewData\\:tabContainer\\:term-planning-container\\:j_id_6m_13_2_0_1\\:collapsiblePanel > div:nth-child(3)')
+# ziehe Organisationseinheit
+Boxinhalt <- Boxinhalt$getElementText()
+
+
 driver$server$stop()
  
